e3ccc6f2d5ccf6868c3ec426dd83480c6ec02ec9
PaoloPascucci <pascuccip9@gmail.com>
2021-10-20 16:29:12 +0200

    Add facile and link css

--- main.js
+++ main.js
@@ -6,4 +6,34 @@ con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 */
+let UserTrial = prompt('Scegli la difficoltà: Facile, Medio o Difficile?')
 
+if (UserTrial == "Facile") {
+    easy()
+}
+else if (UserTrial = "Medio") {
+    medium()
+}
+else if (UserTrial = "Difficile") {
+    Hard()
+}
+else {
+    document.getElementById('container').innerHTML = "Inserire la difficolta, grazie :)"
+}
+
+
+
+
+function easy() {
+    const cont = document.getElementById('container');
+    for (let i = 1; i <= 100; i++) {
+        const Cell = document.createElement("div");
+        Cell.classList.add("gridcell");
+        cont.append(Cell);
+
+        Cell.addEventListener('click', function () {
+            this.style.backgroundColor = "skyblue";
+            // console.log('cliccato');
+        })
+    }
+}
